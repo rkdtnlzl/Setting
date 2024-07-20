bc0e4ea03a5dd5e796efd17a9f7504e67c0c9ee6
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-20 16:13:53 +0900

    [Feat] Observable 객체 생성

---
+++ Setting/Observable.swift
@@ -0,0 +1,26 @@
+//
+//  Observable.swift
+//  Setting
+//
+//  Created by 강석호 on 7/20/24.
+//
+
+import Foundation
+
+class Observable<T> {
+    var closure: ((T) -> Void)?
+    
+    var value: T {
+        didSet {
+            closure?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(closure: @escaping (T) -> Void) {
+        self.closure = closure
+    }
+}
